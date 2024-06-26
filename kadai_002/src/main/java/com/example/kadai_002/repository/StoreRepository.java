3969e4728778f12bc9bfd7795b1d8c805918a052
KotomiKuwata <163006489+KotomiKuwata@users.noreply.github.com>
2024-06-26 12:52:56 +0900

    テーブルとモデルを作成

---
+++ kadai_002/src/main/java/com/example/kadai_002/repository/StoreRepository.java
@@ -0,0 +1,8 @@
+package com.example.kadai_002.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.kadai_002.entity.Store;
+public interface StoreRepository extends JpaRepository<Store, Integer> {
+
+}
