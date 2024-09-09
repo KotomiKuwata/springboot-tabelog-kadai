5137a789814a59bc37f5519e05895bba61039aae
KotomiKuwata <163006489+KotomiKuwata@users.noreply.github.com>
2024-09-09 10:06:40 +0900

    9/9

--- resources/static/js/stripe.js
+++ resources/static/js/stripe.js
@@ -7,3 +7,20 @@ paymentButton.addEventListener('click', () => {
 		sessionId: sessionId
 	})
 });
+/*
+const stripe = Stripe('pk_test_51PjGIyFRY5VG4shkpqJOgbBIKa42NRQ4ZHuP5C03X6btIJnLFaAunpdOBc9t9fc04txhW8BiGFvdjkQIxb15gk0D00fYp1C0Uy');
+const paymentButton = document.getElementById('paymentButton');
+
+paymentButton.addEventListener('click', (e) => {
+	e.preventDefault();
+	const sessionId = paymentButton.getAttribute('data-session-id');
+	stripe.redirectToCheckout({ sessionId: sessionId })
+		.then((result) => {
+			if (result.error) {
+				alert(result.error.message);
+			}
+		})
+		.catch((error) => {
+			console.error('Error:', error);
+		});
+});*/
